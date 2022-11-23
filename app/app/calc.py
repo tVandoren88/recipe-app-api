1855b402aae00e2e69da49ae154aed0c7e1cca6d
tVandoren88 <t.vandoren88@gmail.com>
2022-11-23 11:06:21 -0600

    lesson35 book mark

---
+++ app/app/calc.py
@@ -0,0 +1,12 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return results"""
+    return x + y 
+
+def subtract(x, y):
+    """Subtact x from y and return results"""
+    return y - x
